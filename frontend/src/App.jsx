c1fd11720f90bcf7d529f9d415cec5f1d3e24185
Damianouo <damianouo0@gmail.com>
2025-03-01 22:32:52 +0800

    feat: implement lazy loading, add Loading page

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,21 +1,29 @@
+import { lazy, Suspense } from "react";
 import { createBrowserRouter, RouterProvider, Outlet } from "react-router-dom";
 import Navbar from "./components/navbar/Navbar";
-import Contest from "./pages/Contest";
-import Home from "./pages/Home";
-import BrowseContests, { loader as browseContestsLoader } from "./pages/BrowseContests";
-import CreateContest, { action as creationAction } from "./pages/CreateContest";
-import Error from "./pages/Error";
+const Contest = lazy(() => import("./pages/Contest"));
+const Home = lazy(() => import("./pages/Home"));
+const BrowseContests = lazy(() => import("./pages/BrowseContests"));
+import { loader as browseContestsLoader } from "./pages/BrowseContests";
+const CreateContest = lazy(() => import("./pages/CreateContest"));
+import { action as creationAction } from "./pages/CreateContest";
+const Error = lazy(() => import("./pages/Error"));
 import { rootAction, rootLoader } from "./utils/root";
 import Layout from "./components/layout/Layout";
-import CreationCtxProvider from "./contexts/CreationCtx";
-import Login, { loader as loginLoader } from "./pages/Login";
-import Ranking, { loader as rankingLoader } from "./pages/Ranking";
-import ContestCtxProvider from "./contexts/ContestCtx";
-import CompeteCtxProvider from "./contexts/CompeteCtx";
+const ContestCtxProvider = lazy(() => import("./contexts/ContestCtx"));
+const CompeteCtxProvider = lazy(() => import("./contexts/CompeteCtx"));
+const CreationCtxProvider = lazy(() => import("./contexts/CreationCtx"));
+const Login = lazy(() => import("./pages/Login"));
+import { loader as loginLoader } from "./pages/Login";
+const Ranking = lazy(() => import("./pages/Ranking"));
+import { loader as rankingLoader } from "./pages/Ranking";
 import { action as competeAction } from "./components/contest/ContestCompetePage.";
 import PageBackground from "./components/layout/PageBackground";
-import User, { loader as userLoader } from "./pages/User";
-import Edit, { loader as editLoader, action as editAction } from "./pages/Edit";
+const User = lazy(() => import("./pages/User"));
+import { loader as userLoader } from "./pages/User";
+const Edit = lazy(() => import("./pages/Edit"));
+import { loader as editLoader, action as editAction } from "./pages/Edit";
+import Loading from "./pages/Loading";
 
 const router = createBrowserRouter([
   {
@@ -26,22 +34,38 @@ const router = createBrowserRouter([
     loader: rootLoader,
     action: rootAction,
     children: [
-      { index: true, element: <Home /> },
+      {
+        index: true,
+        element: (
+          <Suspense fallback={<Loading />}>
+            <Home />
+          </Suspense>
+        ),
+      },
       {
         path: "/contests",
         id: "contests",
         loader: browseContestsLoader,
         children: [
-          { index: true, element: <BrowseContests /> },
+          {
+            index: true,
+            element: (
+              <Suspense fallback={<Loading />}>
+                <BrowseContests />
+              </Suspense>
+            ),
+          },
           {
             path: ":contestId",
             action: competeAction,
             element: (
-              <ContestCtxProvider>
-                <CompeteCtxProvider>
-                  <Contest />
-                </CompeteCtxProvider>
-              </ContestCtxProvider>
+              <Suspense fallback={<Loading />}>
+                <ContestCtxProvider>
+                  <CompeteCtxProvider>
+                    <Contest />
+                  </CompeteCtxProvider>
+                </ContestCtxProvider>
+              </Suspense>
             ),
           },
         ],
@@ -50,26 +74,50 @@ const router = createBrowserRouter([
         path: "/create",
         action: creationAction,
         element: (
-          <CreationCtxProvider>
-            <CreateContest />
-          </CreationCtxProvider>
+          <Suspense fallback={<Loading />}>
+            <CreationCtxProvider>
+              <CreateContest />
+            </CreationCtxProvider>
+          </Suspense>
         ),
       },
-      { path: "/login", loader: loginLoader, element: <Login /> },
+      {
+        path: "/login",
+        loader: loginLoader,
+        element: (
+          <Suspense fallback={<Loading />}>
+            <Login />
+          </Suspense>
+        ),
+      },
       {
         path: "/ranking/:contestId",
         loader: rankingLoader,
-        element: <Ranking />,
+        element: (
+          <Suspense fallback={<Loading />}>
+            <Ranking />,
+          </Suspense>
+        ),
       },
-      { path: "/user/:userId", loader: userLoader, element: <User /> },
+      {
+        path: "/user/:userId",
+        loader: userLoader,
+        element: (
+          <Suspense fallback={<Loading />}>
+            <User />
+          </Suspense>
+        ),
+      },
       {
         path: "/edit/:contestId",
         loader: editLoader,
         action: editAction,
         element: (
-          <CreationCtxProvider>
-            <Edit />
-          </CreationCtxProvider>
+          <Suspense fallback={<Loading />}>
+            <CreationCtxProvider>
+              <Edit />
+            </CreationCtxProvider>
+          </Suspense>
         ),
       },
     ],
