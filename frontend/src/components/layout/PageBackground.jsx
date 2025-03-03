a6af238b036fd086a0681bb759aef33bcc33bad0
Damianouo <damianouo0@gmail.com>
2025-03-03 13:35:58 +0800

    fix: fix the issue with page background donsn't look as expected
    update animation utility class in PageBackground
    new animation custom variables in index.css
    replace card-pattern.png with an optimized one
    add border in Navbar to enhance styling

--- frontend/src/components/layout/PageBackground.jsx
+++ frontend/src/components/layout/PageBackground.jsx
@@ -2,7 +2,7 @@ const PageBackground = () => {
   return (
     <>
       <div className="absolute z-0 h-full w-full bg-[#212127]" />
-      <div className="bg-card-pattern animate-bg-pan absolute z-0 h-full w-full bg-[length:300%] sm:bg-[length:auto]" />
+      <div className="bg-card-pattern animate-bg-pan xl:animate-bg-pan-xl lg:animate-bg-pan-lg lg: absolute z-0 h-full w-full bg-[length:300vw] sm:bg-[length:150vw] lg:bg-[length:120vw] xl:bg-[length:110vw]" />
     </>
   );
 };
