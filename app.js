eef8d7681d4a19391217db4bd258650c7e252ca8
Damianouo <damianouo0@gmail.com>
2025-03-04 13:38:09 +0800

    fix: set cookie secure to false for testing

--- app.js
+++ app.js
@@ -34,7 +34,7 @@ app.use(
     saveUninitialized: false,
     cookie: {
       httpOnly: true,
-      secure: process.env.NODE_ENV !== 'dev',
+      secure: false,
       sameSite: 'Lax',
     },
   })
